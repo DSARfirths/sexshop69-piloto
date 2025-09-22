4bbc079d0a3ca2c0b8ad17e7f406fa2c6c3b1590
Diego S. Abad Ramos <DSARfirths@gmail.com>
2025-09-21 22:32:10 -0500

    Piloto 3.4.1

--- app/Tracker.tsx
+++ app/Tracker.tsx
@@ -18,13 +18,7 @@ export default function Tracker() {
       page_path: url,
     });
 
-    // Google Ads (opcional)
-    if (process.env.NEXT_PUBLIC_GADS_ID) {
-      // @ts-ignore
-      window.gtag?.('config', process.env.NEXT_PUBLIC_GADS_ID, {
-        page_path: url,
-      });
-    }
+    
   }, [pathname, searchParams]);
 
   useEffect(() => {
